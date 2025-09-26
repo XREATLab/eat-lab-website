d24df927007f7834d47534929d6da29316c42d54
nmanodept <nmanodept@gmail.com>
2025-09-27 01:56:19 +0800

    T2

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -10,8 +10,15 @@ const { LanguageProvider } = require('./src/contexts/language-context');
 /**
  * @type {import('gatsby').GatsbySSR['onRenderBody']}
  */
-exports.onRenderBody = ({ setHtmlAttributes }) => {
+exports.onRenderBody = ({ setHtmlAttributes, setHeadComponents }) => {
   setHtmlAttributes({ lang: `en` });
+  setHeadComponents([
+    React.createElement("meta", {
+      key: "google-site-verification",
+      name: "google-site-verification",
+      content: "cM-0sqdTxQMHH8VOpKY97GK-6zDc5NII43JTZo37PPo",
+    }),
+  ]);
 };
 
 exports.wrapRootElement = ({ element }) => {
